c6af9fa50f996b83bf0475b7d60f059934ad9326
Dimitaradev <mitkoradost115@gmail.com>
2022-03-05 13:37:26 +0200

    Joint table between chats and ApplicationUser - Messages table

---
+++ TotallyNotOLX/Models/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TotallyNotOLX.Models
+{
+    public class Messages
+    {
+        [Key]
+        public int Id { get; set; }
+        public Chat ChatId { get; set; }
+
+        public string SenderID { get; set; }
+        public ApplicationUser Sender { get; set; }
+
+        public string MessageContent { get; set; }
+        public string TimeSent { get; set; }
+    }
+}
