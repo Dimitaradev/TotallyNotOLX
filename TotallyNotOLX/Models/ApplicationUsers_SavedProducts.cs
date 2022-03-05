0c2f521e0445f91d1e767987ce698e4ea0074f42
Dimitaradev <mitkoradost115@gmail.com>
2022-03-05 13:09:56 +0200

    Joint table between ApplicationUser and Product

---
+++ TotallyNotOLX/Models/ApplicationUsers_SavedProducts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TotallyNotOLX.Models
+{
+    public class ApplicationUsers_SavedProducts
+    {
+        [Key]
+        public int UserID { get; set; }
+        public ApplicationUser User { get; set; }
+
+        [Key]
+        public int ProductID { get; set; }
+        public Product Product { get; set; }
+    }
+}
