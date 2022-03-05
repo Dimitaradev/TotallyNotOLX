5eb4412422ebfcc7b15688d223725569b9e2d92b
Dimitaradev <mitkoradost115@gmail.com>
2022-03-05 12:43:24 +0200

    Created product images table in Models

---
+++ TotallyNotOLX/Models/ProductImage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TotallyNotOLX.Models
+{
+    public class ProductImage
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string URL { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
