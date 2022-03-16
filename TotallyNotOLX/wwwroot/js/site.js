8701ce64f0d9c47c4318c5cf349a5dd1fabaf910
ivailochav <ivailochav@gmail.com>
2022-03-16 22:03:50 +0200

    Added image uploading functionality

--- TotallyNotOLX/wwwroot/js/site.js
+++ TotallyNotOLX/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function clearFormInputs(formId) {
+    document.querySelectorAll('#' + formId+' input').forEach((e) => {
+        if (e.type == "password" || e.type == "text" || e.type == "email") {
+            e.value = "";
+        }
+        else if (e.type == "checkbox") {
+            e.checked = false;
+        }
+        else if (e.type == "file") {
+            e.value = null;
+        }
+    })
+}
